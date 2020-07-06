8d1d98ae647c627e34ee0ed9033b998303964142
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-06 00:32:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
   array_1 = [1, "hello", [], 5.01, "world", :name, { a: 1 }]
   array_2 = [ "", "Hello", 4, [], "", "" ]
-def count_strings(array_1)
+def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   count_strings(array_1)
 end
